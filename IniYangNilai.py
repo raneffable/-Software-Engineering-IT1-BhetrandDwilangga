ee84623574289149cfa07e6695765a8d4aa5fdd3
Bhetrand Dwilangga <73297575+raneffable@users.noreply.github.com>
2021-09-20 18:06:46 +0700

    Add files via upload

---
+++ IniYangNilai.py
@@ -0,0 +1,2 @@
+List_1 = ["David"] * 10
+print(List_1)
---
+++ main.py
@@ -0,0 +1,52 @@
+# variable
+
+# integer
+a = 2
+b = 3
+b = b + a
+
+# string
+animal = "Panda"
+
+# boolean
+c = True
+d = False
+
+# float
+e = 2.0
+
+# List/Array
+f = []
+
+# Dictionary
+g = {}
+
+# Tuple
+h = ()
+
+# Print
+print("Bear")
+print(2 + 5)
+print(True)
+print(b)
+print("Nama Hewannya adalah : " + animal)
+# ini kalo pake dictionary :3
+print("Nama Hewannya adalah :{}".format(animal))
+# contatenate int ke string
+print("Ini nilai a dari int ke str :" + str(a))
+print("Ini yg ga usah di convert : {}".format(a))
+print("Nama : {}, \nTempat Tinggal : {}, \nUmur :{}".format("Ran", "Bengkulu", "19"))
+
+# contoh Array
+NamaOrang = ["David", "David2", "David3"]
+print(NamaOrang)
+print(NamaOrang[0])
+List_1 = ["David"] * 4
+print(List_1)
+print(NamaOrang[0:2]) #ini sebut array 0 dan 1
+print(NamaOrang[-1]) #ini panggil 1 dari terakher
+print(NamaOrang[-2:])#ini 2 terakhir
+
+#nambahkan ke array pake append
+NamaOrang.append("hai")
+print(NamaOrang)
